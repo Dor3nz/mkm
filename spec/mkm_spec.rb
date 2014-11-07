264178890de1fe70968850ce0d7e3c56064cad98
Florian Aßmann <florian@yni.onl>
2014-11-07 09:15:40 +0100

    Added Mkm specs.

--- spec/mkm_spec.rb
+++ spec/mkm_spec.rb
@@ -26,17 +26,23 @@ describe Mkm do
       with(provided_url, instance_of(Hash)).
       and_return faraday
 
-    expect(described_class.connect(provided_url)).to be faraday
+    expect(described_class.connect provided_url).to be faraday
   end
 
   it 'should connect and return an instance of Faraday' do
-    allow(described_class).to receive(:connect).and_return faraday
+    allow(described_class).to receive(:connect).
+      and_return faraday
+
     expect(described_class.connection).to be faraday
   end
 
   it 'should only connect when connection is not set' do
-    allow(Faraday).to receive(:new).and_return(faraday)
-    expect(described_class).to receive(:connect).once.and_call_original
+    allow(Faraday).to receive(:new).
+      and_return faraday
+
+    expect(described_class).to receive(:connect).
+      once.
+      and_call_original
 
     2.times { described_class.connection }
   end
@@ -45,9 +51,15 @@ describe Mkm do
     mkmrc_path = File.join ENV['HOME'], '.mkmrc'
     data = Oj.dump params
 
-    allow(File).to receive(:readable?).with(mkmrc_path).and_return true
-    expect(Oj).to receive(:load_file).with(mkmrc_path).and_return params
-    expect(Mkm::Agent).to receive(:new).with(anything, params)
+    allow(File).to receive(:readable?).
+      and_return true
+
+    expect(Oj).to receive(:load_file).
+      with(mkmrc_path).
+      and_return params
+
+    expect(Mkm::Agent).to receive(:new).
+      with anything, params
 
     described_class.auth
   end
@@ -63,9 +75,30 @@ describe Mkm do
   end
 
   it 'should create a session with an agent' do
+    agent   = double :agent
+    session = double :session
+
+    allow(described_class.const_get :Agent).to receive(:new).
+      and_return agent
+
+    expect(described_class.const_get :Session).to receive(:new).
+      with(agent).
+      and_return session
+
+    expect(described_class.auth params).to be session
   end
 
   it 'should convert Mkm authentication to oauth params' do
+    expect(described_class.const_get :Agent).to receive(:new).
+      with(anything, hash_including(
+        consumer_key: '12345', consumer_secret: '23456',
+        token: '34567', token_secret: '45678'
+      ))
+
+    described_class.auth(
+      app_token: '12345', app_secret: '23456',
+      access_token: '34567', access_token_secret: '45678'
+    )
   end
 
 end
