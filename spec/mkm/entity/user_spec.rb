9ca9a3111e0d29620365a9582bfa553a0d53355c
Florian Aßmann <florian@yni.onl>
2014-11-07 09:43:40 +0100

    Added Mkm::Entity::User specs.

---
+++ spec/mkm/entity/user_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Mkm::Entity::User do
+
+  context 'Commercial Seller' do
+
+    subject { described_class.new samples['seller'] }
+
+    it 'should have the id 9999' do
+      expect(subject.id).to be 9_999
+    end
+
+    it 'should have the username Maxi' do
+      expect(subject.username).to be_eql 'Maxi'
+    end
+
+    it { is_expected.to be_commercial }
+
+    def samples
+      Oj.load_file File.expand_path '../../../samples/seller.json', __FILE__
+    end
+
+  end
+
+end
