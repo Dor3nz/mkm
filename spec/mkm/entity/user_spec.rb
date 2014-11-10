6e0526664f26aa93c9b24f1541d2aeb84ed74cdb
Florian Aßmann <florian@yni.onl>
2014-11-10 16:07:21 +0100

    Added User spec.

--- spec/mkm/entity/user_spec.rb
+++ spec/mkm/entity/user_spec.rb
@@ -11,7 +11,7 @@ describe Mkm::Entity::User do
     end
 
     it 'should have the username Maxi' do
-      expect(subject.username).to be_eql 'Maxi'
+      expect(subject.username).to eq 'Maxi'
     end
 
     it 'should be a commercial user' do
@@ -22,7 +22,7 @@ describe Mkm::Entity::User do
       expect(subject.reliability).to be_reliable
     end
 
-    it 'should have a outstanding reputation' do
+    it 'should have an outstanding reputation' do
       expect(subject.reputation).to be_outstanding
     end
 
@@ -52,6 +52,50 @@ describe Mkm::Entity::User do
       expect(subject.address).not_to be_blank
     end
 
+    context described_class.const_get :Name do
+
+      subject { described_class.new sample('account')['name'] }
+
+      it 'should have the first name "Maximilian"' do
+        expect(subject.first).to eq 'Maximilian'
+      end
+
+      it 'should have the last name "Mustermann"' do
+        expect(subject.last).to eq 'Mustermann'
+      end
+
+
+    end
+    context described_class.const_get :Address do
+
+      subject { described_class.new sample('account')['address'] }
+
+      it 'should have the name "Maximilian Mustermann"' do
+        expect(subject.name).to eq 'Maximilian Mustermann'
+      end
+
+      it 'should have the extra "c/o Musterfrau"' do
+        expect(subject.extra).to eq 'c/o Musterfrau'
+      end
+
+      it 'should have the street "Hauptweg 15"' do
+        expect(subject.street).to eq 'Hauptweg 15'
+      end
+
+      it 'should have the zip code "12345"' do
+        expect(subject.zip).to eq "12345"
+      end
+
+      it 'should have the city "Berlin"' do
+        expect(subject.city).to eq "Berlin"
+      end
+
+      it 'should have the country "D"' do
+        expect(subject.country).to eq 'D'
+      end
+
+
+    end
   end
 
   context 'Commercial Seller' do
@@ -63,7 +107,7 @@ describe Mkm::Entity::User do
     end
 
     it 'should have the username Maxi' do
-      expect(subject.username).to be_eql 'Maxi'
+      expect(subject.username).to eq 'Maxi'
     end
 
     it 'should be a commercial user' do
@@ -74,20 +118,34 @@ describe Mkm::Entity::User do
       expect(subject.reliability).to be_reliable
     end
 
-    it { is_expected.not_to be_away }
-
-    it 'should have an empty name' do
-      expect(subject.name).to be_an described_class.const_get :Name
-      expect(subject.name).to be_blank
+    it 'should have an outstanding reputation' do
+      expect(subject.reputation).to be_outstanding
     end
 
-    it 'should have an empty address' do
-      expect(subject.address).to be_an described_class.const_get :Address
-      expect(subject.address).to be_blank
+    it 'should ship very fast' do
+      expect(subject.shipping_speed).to be_very_fast
+    end
+
+    it 'should have a sale count of 254' do
+      expect(subject.sale_count).to be 254
     end
 
     it 'should not have a balance' do
       expect(subject.balance).to be_nil
+    end
+
+    it { is_expected.not_to be_away }
+
+    it 'should have a language'
+
+    it 'should have a blank name' do
+      expect(subject.name).to be_an described_class.const_get :Name
+      expect(subject.name).to be_blank
+    end
+
+    it 'should have a blank address' do
+      expect(subject.address).to be_an described_class.const_get :Address
+      expect(subject.address).to be_blank
     end
 
   end
