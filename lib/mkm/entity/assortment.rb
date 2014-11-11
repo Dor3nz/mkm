f421338596265e12115dadca3a20953d3664acb6
Florian Aßmann <florian@yni.onl>
2014-11-11 09:30:17 +0100

    Removed obsolete files.

--- lib/mkm/entity/assortment.rb
+++
@@ -1,9 +0,0 @@
-module Mkm
-  class Assortments < Struct.new(:agent)
-
-    def articles
-      @articles ||= agent.get('stock')['article'].map { |data| AssortmentArticle.new agent, data }
-    end
-
-  end
-end
