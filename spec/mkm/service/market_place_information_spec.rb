48f40647177a090db2758f90fc5d7e81b5a797ac
Florian Aßmann <florian@yni.onl>
2014-11-11 09:30:32 +0100

    Simplified MarketPlaceInformation specs.

--- spec/mkm/service/market_place_information_spec.rb
+++ spec/mkm/service/market_place_information_spec.rb
@@ -3,22 +3,18 @@ require 'spec_helper'
 describe Mkm::Service::MarketPlaceInformation do
 
   let(:agent)   { double :agent }
-  let(:data)    { Hash.new { |h, k| h } }
   let(:context) { double :context }
 
   subject { described_class.new context, agent }
 
   it 'should find user by id' do
-    user = double :user
+    data = sample 'seller'
 
     expect(agent).to receive(:get).
       with('user/9999').
-      and_return data
-    expect(Mkm::Entity::User).to receive(:new).
-      with(data).
-      and_return user
+      and_return 'user' => data
 
-    expect(subject.find_user_by_id 9999).to be user
+    expect(subject.find_user_by_id 9999).to eq Mkm::Entity::User.new data
   end
 
   it 'should get games' do
@@ -28,15 +24,7 @@ describe Mkm::Service::MarketPlaceInformation do
       with('games').
       and_return 'game' => [data]
 
-    expect(Mkm::Entity::Game).to receive(:new).
-      once.
-      and_call_original
-
-    games = subject.get_games
-
-    expect(games.length).to be 1
-    expect(games.first).to be_a Mkm::Entity::Game
-    expect(games.first.data).to eq data
+    expect(subject.get_games).to eq [ Mkm::Entity::Game.new(data) ]
   end
 
 end
