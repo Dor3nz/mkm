82d7015732a1e19e3a76827c75a236adf354008c
Florian Aßmann <florian@yni.onl>
2014-11-11 10:00:23 +0100

    Simplified MarketPlaceInformation spec.

--- spec/mkm/service/market_place_information_spec.rb
+++ spec/mkm/service/market_place_information_spec.rb
@@ -8,23 +8,22 @@ describe Mkm::Service::MarketPlaceInformation do
   subject { described_class.new context, agent }
 
   it 'should find user by id' do
-    data = sample 'seller'
-
-    expect(agent).to receive(:get).
+    response = sample 'seller'
+    allow(agent).to receive(:get).
       with('user/9999').
-      and_return 'user' => data
+      and_return 'user' => response
 
-    expect(subject.find_user_by_id 9999).to eq Mkm::Entity::User.new data
+    expect(subject.find_user_by_id 9999).to eq Mkm::Entity::User.new response
   end
 
   it 'should get games' do
-    data = sample('game').first
-
-    expect(agent).to receive(:get).
+    response = sample 'game'
+    allow(agent).to receive(:get).
       with('games').
-      and_return 'game' => [data]
+      and_return 'game' => response
 
-    expect(subject.get_games).to eq [ Mkm::Entity::Game.new(data) ]
+    expect(subject.get_games).to eq response.
+      map { |data| Mkm::Entity::Game.new data }
   end
 
 end
