95f4ebfcd842e38242ca83ae2cefec5af9fd035a
Dor3nz <genis.matutes@gmail.com>
2015-04-01 13:10:12 +0200

    Allow reading agent from session

--- lib/mkm/session.rb
+++ lib/mkm/session.rb
@@ -1,12 +1,11 @@
 module Mkm
   class Session
 
-    attr_reader :account
+    attr_reader :account, :agent
 
     def initialize(connection, authentication)
       @agent = Agent.new connection, authentication
       @account = Account.new @agent
     end
-
   end
 end
